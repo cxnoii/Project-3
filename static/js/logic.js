e605cba1b2c7013b47c242a1c7d013185c213828
chika <chikacokam@gmail.com>
2023-02-08 22:43:02 -0800

    update

--- static/js/logic.js
+++ static/js/logic.js
@@ -11,7 +11,7 @@ function main() {
     }).addTo(map);
     
     let filepath_a = "../../Data/criminal_homicide_2010_2019.json";
-    let filepath_b = "/Project-3/Data/criminal_homicide_2010_2019.json";
+    // let filepath_b = "/Project-3/Data/criminal_homicide_2010_2019.json";
 
     d3.json(filepath_a).then(function (x) {
         console.log(x);
