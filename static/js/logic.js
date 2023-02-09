e51fd8a17c8313f9526e5507c004758bf0dd4969
Nicholas Dao <nickdao86@gmail.com>
2023-02-08 21:04:27 -0800

    added bindpopups

--- static/js/logic.js
+++ static/js/logic.js
@@ -10,31 +10,37 @@ function main() {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
     
-    let filepath_a = "../../Data/criminal_homicide_2010_2019.json"
-    let filepath_b = "/Project-3/Data/criminal_homicide_2010_2019.json"
-    
+    let filepath_a = "../../Data/criminal_homicide_2010_2019.json";
+    let filepath_b = "/Project-3/Data/criminal_homicide_2010_2019.json";
+
     d3.json(filepath_a).then(function (x) {
         console.log(x);
-        drawMarkers(x)
+        drawMarkers(x);
         })
     }
 
 
 function drawMarkers(homicides) {
+    console.log("data passed into drawMarkers", homicides)
     var cases = L.markerClusterGroup();
 
 //looping through each object in array
     for (var i =0; i<homicides.length; i++) {
-        var lat = homicides[i]["LAT"]
-        var lng = homicides[i]["LON"]
+        var lat = homicides[i]["LAT"];
+        var lng = homicides[i]["LON"];
 
         //creating markers for each object
         var marker = L.marker([lat,lng])
+        .bindPopup(`<b>${homicides[i]['LOCATION']}</b> </br> <hr>
+            Date Occured: ${homicides[i]['DATE OCC']} </br>
+            Victim: ${homicides[i]['Vict Age']} / ${homicides[i]['Vict Sex']} </br>
+            Status: ${homicides[i]['Status Desc']}`
+            );
 
-        cases.addLayer(marker)
+        cases.addLayer(marker);
     }
     // initializing a layer group for the array of circle markers and passing it into the createMap function
-    createMap(cases)
+    createMap(cases);
 }
 
 
